d8ef55d936317e8dde67a2ab11278d1be594fb80
Jesse <woah@yehrye.com>
2016-06-15 16:22:29 -0700

    heh heh

--- server/message_handlers/chatterbox.js
+++ server/message_handlers/chatterbox.js
@@ -6,7 +6,11 @@ var server_settings = require('../managers/server_settings');
 exports.handle_message = function handle_message(session, message) {
     var sub_type = message.sub_type;
     var data = message.data;
-    var room_id = data.room_id;
+    var room = wiseau.get_room(data.room_id);
+
+    if (room == null) {
+        return;
+    }
 
     var handle = {
         chat: function() {
@@ -15,17 +19,16 @@ exports.handle_message = function handle_message(session, message) {
             clean_message = clean_message.replace(/>/g, '&gt;');
 
             if (clean_message.length > 0) {
-                var room = wiseau.get_room(room_id);
                 var recent_message = session.profile.username + ': ' + clean_message;
                 room.add_recent_message(recent_message);
 
-                sessionator.broadcast('chatterbox', 'chat', {message: clean_message, username: session.profile.username}, {room_id: room_id});
+                sessionator.broadcast('chatterbox', 'chat', {message: clean_message, username: session.profile.username}, {room_id: room.id});
             }
         },
         blargh: function() {
             if (data.message.length > 0) {
                 var room = wiseau.get_room(room_id);
-                sessionator.broadcast('chatterbox', 'blargh', {message: data.message, username: session.profile.username}, {room_id: room_id});
+                sessionator.broadcast('chatterbox', 'blargh', {message: data.message, username: session.profile.username}, {room_id: room.id});
             }
         },
         change_room_name: function() {
@@ -35,7 +38,7 @@ exports.handle_message = function handle_message(session, message) {
             }
 
             room.name = data.name;
-            sessionator.broadcast('chatterbox', 'change_room_name', {new_name: room.name, blame: session.profile.username}, {room_id: room_id});
+            sessionator.broadcast('chatterbox', 'change_room_name', {new_name: room.name, blame: session.profile.username}, {room_id: room.id});
             server_settings.set('lobby_room_name', data.name);
         }
     };
