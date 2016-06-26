03cfac941385a93167e1a3a74c03309456cfcda0
Jesse <woah@yehrye.com>
2016-06-26 12:49:05 -0700

    configure it out s'more

---
+++ server/conf/sql/db_create.sql
@@ -0,0 +1,23 @@
+CREATE TABLE user (
+	user_id	INTEGER NOT NULL,
+	username	TEXT NOT NULL,
+	password	TEXT NOT NULL,
+	auth_key	TEXT DEFAULT NULL,
+	is_admin	INTEGER NOT NULL DEFAULT 0,
+	email_address	TEXT,
+	salty	TEXT,
+	last_ip_address	TEXT,
+	PRIMARY KEY(user_id)
+);
+
+CREATE TABLE user_avatar (
+	user_id	INTEGER NOT NULL UNIQUE,
+	user_avatar	BLOB,
+	PRIMARY KEY(user_id)
+);
+
+CREATE TABLE user_settings (
+	user_id	INTEGER NOT NULL,
+	settings_json	TEXT,
+	PRIMARY KEY(user_id)
+);
---
+++ shared/shared_config.js
@@ -0,0 +1,7 @@
+// These things should be synced between the server and client!
+module.exports = {
+    http_port: 1991,
+    chat_port: 2001,
+    binary_port: 2112
+};
+// Everything is fine.
