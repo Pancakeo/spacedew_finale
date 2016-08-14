e215d9568b5794b2612658b4ea24ebc88e6be561
Jesse <woah@yehrye.com>
2016-08-14 16:20:15 -0700

    great

--- client/js/pages/black_board.js
+++ client/js/pages/black_board.js
@@ -1,5 +1,18 @@
 module.exports = function() {
     get_page('black_board', function(page) {
+        $('body').append(page.$container);
 
+        var ctx = page.$("#black_board_canvas")[0].getContext('2d');
+        ctx.strokeStyle = "#FF0000";
+
+        var resize_canvas = function() {
+            var height = $(window).height();
+            var width = $(window).width();
+            page.$("#black_board_canvas").attr({width: width, height: height});
+        };
+
+        resize_canvas();
+
+        $(window).on('resize', resize_canvas);
     });
 };
