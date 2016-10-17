e8c4c6899fbaf295cfd44a09f9ac8199797adec8
Jesse <woah@yehrye.com>
2016-10-17 08:03:38 -0700

    Seizerues

--- client/js/app/default_emus.js
+++ client/js/app/default_emus.js
@@ -1,7 +1,7 @@
 var emus = {
     0: {team_play: true, text: 'Whew.'},
     1: {text: 'Whew.'},
-    2: {text: 'Does it... Does it say cheethos are not for your eyes?'},
+    2: {text: 'Does it... Does it say cheetos are not for your eyes?'},
     3: {text: 'Holy cow!'},
     4: {text: 'What a play!'},
     5: {text: 'Nice one!'},
