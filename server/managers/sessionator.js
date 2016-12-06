92e47622908162e6ae8fec65c5cf7183b3c1d208
Jesse <woah@yehrye.com>
2016-12-06 13:23:00 -0800

    Security

--- server/managers/sessionator.js
+++ server/managers/sessionator.js
@@ -59,7 +59,8 @@ exports.broadcast = function(type, sub_type, data, options) {
         if (options.require_logged_in && session.logged_in) {
             if (room != null) {
 
-                if (room.is_member(session.profile.username) >= 0) {
+                if (room.is_member(session.profile.username)) {
+                    console.log('send to', session.profile.username, options.room_id);
                     session.send(type, sub_type, data, {strip_entities: options.strip_entities});
                 }
             }
