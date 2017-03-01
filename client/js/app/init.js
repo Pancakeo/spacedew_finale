95ce8968be3e73f4b6a93fbd948b0e8cd3bfd8a7
Jesse <woah@yehrye.com>
2017-02-28 16:48:31 -0800

    Quick test

--- client/js/app/init.js
+++ client/js/app/init.js
@@ -50,8 +50,8 @@ module.exports = (function() {
     var shared_config = require('../../../shared/shared_config'); // TODO - consider copying shared/* to a more relative folder.
 
     var server_settings = {
-        server: 'ws://localhost:' + shared_config.chat_port,
-        binary_server: 'ws://localhost:' + shared_config.binary_port
+        server: 'wss://localhost:' + shared_config.chat_port,
+        binary_server: 'wss://localhost:' + shared_config.binary_port
     };
 
     for (var server in server_settings) {
