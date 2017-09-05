cbd5baf5fddd0b943b987a6152f671fc7c2ec3dd
thegauntt <jessethegauntt+biz@gmail.com>
2017-09-04 18:27:14 -0700

    Probably responsive

--- client/js/pages/chatterbox.js
+++ client/js/pages/chatterbox.js
@@ -377,15 +377,16 @@ export default function ($parent, options) {
 		}
 		
 		var buttsFactory = new ButtsFactory();
-		var butts;
-		butts = buttsFactory.newButts(10, 5);
+		var butts = buttsFactory.newButts(10, 5);
 		
 		page.peepy('users.roams_the_earth', function (event) {
-			append_system(event.username + butts.butter(" roams the earth. Diablo's minions grow stronger."), { class_name: 'happy', room_id: app.get_lobby(true) })
+			// append_system(event.username + butts.butter(" roams the earth. Diablo's minions grow stronger."), { class_name: 'happy', room_id: app.get_lobby(true) })
+			append_system(event.username + " roams the earth. Diablo's minions grow stronger.", { class_name: 'happy', room_id: app.get_lobby(true) })
 		});
 
 		page.peepy('users.has_gone_to_a_better_place', function (event) {
-			append_system(event.username + butts.butter(" went to the clearing at the end of the path. Diablo's minions are mildly frustrated."), { class_name: 'sad', room_id: app.get_lobby(true) })
+			// append_system(event.username + butts.butter(" went to the clearing at the end of the path. Diablo's minions are mildly frustrated."), { class_name: 'sad', room_id: app.get_lobby(true) })
+			append_system(event.username + " went to the clearing at the end of the path. Diablo's minions are mildly frustrated.", { class_name: 'sad', room_id: app.get_lobby(true) })
 		});
 
 		event_bus.on('blargher.send', function (params) {
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,5 @@
 const {
-  resolve
+	resolve
 } = require('path');
 
 const Clean = require('clean-webpack-plugin');
@@ -9,207 +9,209 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 let devConfig = {
-  // context: resolve(__dirname, 'client'),
+	// context: resolve(__dirname, 'client'),
 
-  entry: [
-    'react-hot-loader/patch',
-    // activate HMR for React
+	entry: [
+		'react-hot-loader/patch',
+		// activate HMR for React
 
-    'webpack-dev-server/client?http://localhost:8080',
-    // bundle the client for webpack-dev-server
-    // and connect to the provided endpoint
+		'webpack-dev-server/client?http://localhost:8080',
+		// bundle the client for webpack-dev-server
+		// and connect to the provided endpoint
 
-    'webpack/hot/only-dev-server',
-    // bundle the client for hot reloading
-    // only- means to only hot reload for successful updates
+		'webpack/hot/only-dev-server',
+		// bundle the client for hot reloading
+		// only- means to only hot reload for successful updates
 
-    './js/app/init.jsx'
-    // the entry point of our app
-  ],
-  output: {
-    filename: 'spacedew_finale.js',
-    // the output bundle
+		'./js/app/init.jsx'
+		// the entry point of our app
+	],
+	output: {
+		filename: 'spacedew_finale.js',
+		// the output bundle
 
-    path: resolve(__dirname, 'dist'),
+		path: resolve(__dirname, 'dist'),
 
-    publicPath: '/'
-    // necessary for HMR to know where to load the hot update chunks
-  },
+		publicPath: '/'
+		// necessary for HMR to know where to load the hot update chunks
+	},
 
-  // devtool: 'inline-source-map',
-  devtool: 'source-map',
+	// devtool: 'inline-source-map',
+	devtool: 'source-map',
 
-  devServer: {
-    hot: true,
-    // enable HMR on the server
+	devServer: {
+		hot: true,
+		// enable HMR on the server
 
-    contentBase: resolve(__dirname, 'dist'),
-    // match the output path
+		contentBase: resolve(__dirname, 'dist'),
+		// match the output path
 
-    publicPath: '/'
-    // match the output `publicPath`
-  },
+		publicPath: '/'
+		// match the output `publicPath`
+	},
 
-  module: {
-    rules: [{
-        test: /\.jsx/,
-        loader: 'babel-loader',
-        exclude: [
-          resolve(__dirname, "node_modules")
-        ],
-      }, {
-        test: /\.(png|jpg|gif)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8192
-          }
-        }]
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader"
-      },
-      {
-        test: /\.(less|css)$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader',
-            {
-              loader: 'less-loader',
-              options: {
-                noIeCompat: true
-              }
-            }
-          ]
-        })
-      }
-    ]
-  },
-  plugins: [
-    new Clean(['dist']),
+	module: {
+		rules: [{
+				test: /\.jsx/,
+				loader: 'babel-loader',
+				exclude: [
+					resolve(__dirname, "node_modules")
+				],
+			}, {
+				test: /\.(png|jpg|gif)$/,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						limit: 8192
+					}
+				}]
+			},
+			{
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "url-loader?limit=10000&mimetype=application/font-woff"
+			},
+			{
+				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "file-loader"
+			},
+			{
+				test: /\.(less|css)$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader',
+						{
+							loader: 'less-loader',
+							options: {
+								noIeCompat: true
+							}
+						}
+					]
+				})
+			}
+		]
+	},
+	plugins: [
+		new Clean(['dist']),
 
-    new webpack.HotModuleReplacementPlugin(),
-    // enable HMR globally
+		new webpack.HotModuleReplacementPlugin(),
+		// enable HMR globally
 
-    new webpack.NamedModulesPlugin(),
-    // prints more readable module names in the browser console on HMR updates
+		new webpack.NamedModulesPlugin(),
+		// prints more readable module names in the browser console on HMR updates
 
-    new CopyWebpackPlugin([{
-        from: './html',
-        to: 'html'
-      },
-      {
-        from: './js/public',
-        to: 'js/public'
-      },
-      {
-        from: './public',
-        to: 'public'
-      }
-    ]),
+		new CopyWebpackPlugin([{
+				from: './html',
+				to: 'html'
+			},
+			{
+				from: './js/public',
+				to: 'js/public'
+			},
+			{
+				from: './public',
+				to: 'public'
+			}
+		]),
 
-    new ExtractTextPlugin("wup.css"),
+		new ExtractTextPlugin("wup.css"),
 
-    new HtmlWebpackPlugin({
-      title: "Bourbon 'N' Rye (Dev)",
-      hash: true,
-      favicon: './public/favicon-normal.png'
-    })
-  ]
+		new HtmlWebpackPlugin({
+			template: 'index_template.ejs',
+			title: "Reckless Abandon (Dev)",
+			hash: true,
+			favicon: './public/favicon-normal.png'
+		})
+	]
 };
 
 let prodConfig = {
-  entry: [
-    // the entry point of our app
-    './js/app/init.jsx'
-  ],
-  output: {
-    filename: 'spacedew_finale.js',
-    path: resolve(__dirname, 'dist')
-  },
+	entry: [
+		// the entry point of our app
+		'./js/app/init.jsx'
+	],
+	output: {
+		filename: 'spacedew_finale.js',
+		path: resolve(__dirname, 'dist')
+	},
 
-  // devtool: 'nosources-source-map',
+	// devtool: 'nosources-source-map',
 
-  module: {
-    rules: [{
-        test: /\.jsx/,
-        loader: 'babel-loader'
-      }, {
-        test: /\.(png|jpg|gif)$/,
-        use: [{
-          loader: 'url-loader',
-          options: {
-            limit: 8192
-          }
-        }]
-      },
-      {
-        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "url-loader?limit=10000&mimetype=application/font-woff"
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        loader: "file-loader"
-      },
-      {
-        test: /\.(less|css)$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader',
-            {
-              loader: 'less-loader',
-              options: {
-                noIeCompat: true
-              }
-            }
-          ]
-        })
-      }
-    ]
-  },
-  plugins: [
-    new Clean(['dist']),
+	module: {
+		rules: [{
+				test: /\.jsx/,
+				loader: 'babel-loader'
+			}, {
+				test: /\.(png|jpg|gif)$/,
+				use: [{
+					loader: 'url-loader',
+					options: {
+						limit: 8192
+					}
+				}]
+			},
+			{
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "url-loader?limit=10000&mimetype=application/font-woff"
+			},
+			{
+				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "file-loader"
+			},
+			{
+				test: /\.(less|css)$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: ['css-loader',
+						{
+							loader: 'less-loader',
+							options: {
+								noIeCompat: true
+							}
+						}
+					]
+				})
+			}
+		]
+	},
+	plugins: [
+		new Clean(['dist']),
 
-    new webpack.DefinePlugin({
-      'process.env': {
-        // This has effect on the react lib size
-        'NODE_ENV': JSON.stringify('production'),
-      }
-    }),
+		new webpack.DefinePlugin({
+			'process.env': {
+				// This has effect on the react lib size
+				'NODE_ENV': JSON.stringify('production'),
+			}
+		}),
 
-    new CopyWebpackPlugin([{
-        from: './html',
-        to: 'html'
-      },
-      {
-        from: './js/public',
-        to: 'js/public'
-      },
-      {
-        from: './public',
-        to: 'public'
-      }
-    ]),
+		new CopyWebpackPlugin([{
+				from: './html',
+				to: 'html'
+			},
+			{
+				from: './js/public',
+				to: 'js/public'
+			},
+			{
+				from: './public',
+				to: 'public'
+			}
+		]),
 
-    new ExtractTextPlugin("wup.css"),
+		new ExtractTextPlugin("wup.css"),
 
-    new HtmlWebpackPlugin({
-      title: "Bourbon 'N' Rye",
-      hash: true,
-      favicon: './public/favicon-normal.png'
-    })
-  ]
+		new HtmlWebpackPlugin({
+			template: 'index_template.ejs',
+			title: "Reckless Abandon",
+			hash: true,
+			favicon: './public/favicon-normal.png'
+		})
+	]
 };
 
 if (process.env.NODE_ENV && process.env.NODE_ENV.trim() == 'production') {
-  console.log("Using prod config.");
-  module.exports = prodConfig;
+	console.log("Using prod config.");
+	module.exports = prodConfig;
 } else {
-  console.log("Using dev config.");
-  module.exports = devConfig;
+	console.log("Using dev config.");
+	module.exports = devConfig;
 }
